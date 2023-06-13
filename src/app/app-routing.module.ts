bdcbb2105ffdbf19331d98ea2843eaf8b0327638
NemuiLiz <nemui.liz@gmx.de>
2023-06-13 09:21:37 +0200

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
+import {DatenschutzComponent} from "./components/datenschutz/datenschutz.component";
+import {ImpressumComponent} from "./components/impressum/impressum.component";
+import {LoginComponent} from "./components/login/login.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {CmpNotFoundComponent} from "./components/cmp-not-found/cmp-not-found.component";
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+  const routes: Routes = [
+      { path: '', component: DashboardComponent },
+      { path: 'datenschutz', component: DatenschutzComponent },
+      { path: 'impressum', component: ImpressumComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '**', component: CmpNotFoundComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
+import { DatenschutzComponent } from './components/datenschutz/datenschutz.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CmpNotFoundComponent } from './components/cmp-not-found/cmp-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    TodoListComponent
+    TodoListComponent,
+    LoginComponent,
+    ImpressumComponent,
+    DatenschutzComponent,
+    DashboardComponent,
+    CmpNotFoundComponent
   ],
   imports: [
     BrowserModule,
